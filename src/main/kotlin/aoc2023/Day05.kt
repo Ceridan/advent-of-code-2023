ac3bdda431b227f556be1992b30396f7d754b0ec
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2023-12-05 21:16:03 +0100

    feat: Solution for Day 05, part2.

--- src/main/kotlin/aoc2023/Day05.kt
+++ src/main/kotlin/aoc2023/Day05.kt
@@ -26,7 +26,58 @@ class Day05 {
     }
 
     fun part2(input: String): Long {
-        return 0L
+        val almanac = parseAlmanac(input)
+        var key = "seed"
+        var items = almanac.seeds.windowed(2, 2).map { LongRange(it[0], it[0] + it[1]) }.toSet()
+
+        while (key in almanac.keys) {
+            val keyPair = key to almanac.keys[key]!!
+            val newItems = mutableSetOf<LongRange>()
+
+            val queue = ArrayDeque(items)
+            queue@ while (queue.isNotEmpty()) {
+                val range = queue.removeFirst()
+                val transforms = almanac.transforms.getOrDefault(keyPair, listOf())
+                for (transform in transforms) {
+                    val (left, intersection, right) = range.splitIntersect(transform.source)
+                    if (intersection != null) {
+                        val dest = LongRange(
+                            transform.destination.first + (intersection.first - transform.source.first),
+                            transform.destination.first + (intersection.last - transform.source.first)
+                        )
+                        newItems.add(dest)
+                        if (left != null) queue.addFirst(left)
+                        if (right != null) queue.addFirst(right)
+                        continue@queue
+                    }
+                }
+                newItems.add(range)
+            }
+            items = newItems
+            key = almanac.keys.getOrDefault(key, "")
+        }
+
+        return items.minOf { it.first }
+    }
+
+    private fun LongRange.splitIntersect(other: LongRange): Triple<LongRange?, LongRange?, LongRange?> {
+        var left: LongRange? = null
+        var intersect: LongRange? = null
+        var right: LongRange? = null
+
+        if (first < other.first) {
+            left = LongRange(first, last.coerceAtMost(other.first - 1L))
+        }
+
+        if (last > other.last) {
+            right = LongRange(first.coerceAtLeast(other.last + 1L), last)
+        }
+
+        if ((other.first in first..last) || (first in other.first..other.last)) {
+            intersect = LongRange(first.coerceAtLeast(other.first), last.coerceAtMost(other.last))
+        }
+
+        return Triple(left, intersect, right)
     }
 
     private fun parseAlmanac(almanac: String): Almanac {
@@ -58,11 +109,11 @@ class Day05 {
             }
         }
 
-        return Almanac(seeds, transforms, transforms.keys.associateBy({it.first}, {it.second}))
+        return Almanac(seeds, transforms, transforms.keys.associateBy({ it.first }, { it.second }))
     }
 
-    data class Almanac(val seeds: List<Long>, val transforms: Transform, val keys: Map<String, String>) {}
-    data class TransformEntry(val source: LongRange, val destination: LongRange) {}
+    data class Almanac(val seeds: List<Long>, val transforms: Transform, val keys: Map<String, String>)
+    data class TransformEntry(val source: LongRange, val destination: LongRange)
 }
 
 fun main() {
