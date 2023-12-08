eab22216df267056f2d4c7abce2a8224044378e6
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2023-12-08 09:55:26 +0100

    feat: Naive solution. Will work for ages.

--- src/main/kotlin/aoc2023/Day08.kt
+++ src/main/kotlin/aoc2023/Day08.kt
@@ -2,40 +2,46 @@ package aoc2023
 
 class Day08 {
     fun part1(input: List<String>): Int {
-        val navigation = parseInput(input)
+        val (directions, network) = parseInput(input)
         var node = "AAA"
+        var steps = 0
+        var directionIndex = 0
         while (node != "ZZZ") {
-            node = navigation.nextNode(node)
+            node = if (directions[directionIndex] == 'L') network[node]!!.first else network[node]!!.second
+            directionIndex = (directionIndex + 1) % directions.size
+            steps += 1
         }
-        return navigation.steps
+        return steps
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val (directions, network) = parseInput(input)
+        val current = network.keys.filter { it.endsWith('A') }.toTypedArray()
+        val zNodes = network.keys.filter { it.endsWith('Z') }
+        var steps = 0
+        var zCount = 0
+        var directionIndex = 0
+        while (zCount < zNodes.size) {
+            zCount = 0
+            for (i in current.indices) {
+                current[i] = if (directions[directionIndex] == 'L') network[current[i]]!!.first else network[current[i]]!!.second
+                if (current[i] in zNodes) zCount += 1
+            }
+            directionIndex = (directionIndex + 1) % directions.size
+            steps += 1
+        }
+        return steps
     }
 
-    private fun parseInput(input: List<String>): Navigation {
+    private fun parseInput(input: List<String>): Pair<CharArray, Map<String, Pair<String, String>>> {
         val directions = input[0].toCharArray()
         val network = mutableMapOf<String, Pair<String, String>>()
-        val nodeRegex = "^([A-Z]{3}) = \\(([A-Z]{3}), ([A-Z]{3})\\)".toRegex()
+        val nodeRegex = "^([A-Z0-9]{3}) = \\(([A-Z0-9]{3}), ([A-Z0-9]{3})\\)".toRegex()
         for (i in 2..<input.size) {
             val (node, nodeL, nodeR) = nodeRegex.find(input[i])!!.destructured
             network[node] = nodeL to nodeR
         }
-        return Navigation(directions, network)
-    }
-
-    class Navigation(private val directions: CharArray, private val network: Map<String, Pair<String, String>>) {
-        var steps = 0
-        private var directionIndex = 0
-
-        fun nextNode(node: String): String {
-            val nextNode = if (directions[directionIndex] == 'L') network[node]!!.first else network[node]!!.second
-
-            directionIndex = (directionIndex + 1) % directions.size
-            steps += 1
-            return nextNode
-        }
+        return directions to network
     }
 }
 
