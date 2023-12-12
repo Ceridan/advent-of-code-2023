efd5997f8573d5f3be3ec68b87b727faca1e6818
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2023-12-12 23:18:34 +0100

    feat: Naive approach for day 12, part 2 - timeout.

--- src/main/kotlin/aoc2023/Day12.kt
+++ src/main/kotlin/aoc2023/Day12.kt
@@ -2,27 +2,32 @@ package aoc2023
 
 class Day12 {
     fun part1(input: String): Long {
-        val (rows, codes) = parseInput(input)
-        return rows.zip(codes).sumOf { (r, c) -> calculateArrangements(r, c) }
+        val (rows, groups) = parseInput(input)
+        return rows.zip(groups).sumOf { (r, c) -> calculateArrangements(r, c) }
     }
 
     fun part2(input: String): Long {
-        return 0
+        val (rows, groups) = parseInput(input)
+        val expandedRows = rows.map { "$it?".repeat(5).dropLast(1) }
+        val expandedGroups = groups.map { group ->
+            group.joinToString(separator = ",", postfix = ",").repeat(5).dropLast(1).split(',').map { it.toInt() }
+        }
+        return expandedRows.zip(expandedGroups).sumOf { (r, c) -> calculateArrangements(r, c) }
     }
 
     private fun calculateArrangements(
         pattern: String,
-        codes: List<Int>,
+        groups: List<Int>,
         rowIdx: Int = 0,
         colIdx: Int = 0,
         parts: Map<Int, Int> = mapOf()
     ): Long {
-        if (colIdx == codes.size) {
+        if (colIdx == groups.size) {
             val arrangement = createArrangement(pattern, parts)
-            return if (isValidArrangement(arrangement, pattern, codes)) 1L else 0L
+            return if (isValidArrangement(arrangement, pattern, groups)) 1L else 0L
         }
 
-        return pattern.drop(rowIdx).windowed(codes[colIdx], 1).withIndex()
+        return pattern.drop(rowIdx).windowed(groups[colIdx], 1).withIndex()
             .filter { (i, window) ->
                 if (window.any { it == '.' }) return@filter false
 
@@ -35,7 +40,7 @@ class Day12 {
             .sumOf { (i, window) ->
                 calculateArrangements(
                     pattern,
-                    codes,
+                    groups,
                     rowIdx + window.length + i + 1,
                     colIdx + 1,
                     mapOf(rowIdx + i to window.length) + parts
@@ -53,9 +58,9 @@ class Day12 {
         return chars.joinToString(separator = "")
     }
 
-    private fun isValidArrangement(variant: String, pattern: String, codes: List<Int>): Boolean {
-        val rowCodes = variant.split('.').filter { it.isNotEmpty() }.map { it.length }
-        if (rowCodes != codes) return false
+    private fun isValidArrangement(variant: String, pattern: String, groups: List<Int>): Boolean {
+        val rowgroups = variant.split('.').filter { it.isNotEmpty() }.map { it.length }
+        if (rowgroups != groups) return false
 
         pattern.zip(variant).forEach { (p, v) ->
             if (p != '?' && p != v) return false
@@ -66,14 +71,14 @@ class Day12 {
 
     private fun parseInput(input: String): Pair<List<String>, List<List<Int>>> {
         val rows = mutableListOf<String>()
-        val codes = mutableListOf<List<Int>>()
+        val groups = mutableListOf<List<Int>>()
         val lines = input.split('\n').filter { it.isNotEmpty() }
         for (line in lines) {
-            val (rowString, codeString) = line.split(' ')
+            val (rowString, groupString) = line.split(' ')
             rows.add(rowString)
-            codes.add(codeString.split(',').map { it.toInt() })
+            groups.add(groupString.split(',').map { it.toInt() })
         }
-        return rows to codes
+        return rows to groups
     }
 }
 
