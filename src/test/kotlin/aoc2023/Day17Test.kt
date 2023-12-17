53105b33ac9c44f4988c531b18f698329c262066
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2023-12-17 14:50:37 +0100

    feat: Add tests for part 2.

--- src/test/kotlin/aoc2023/Day17Test.kt
+++ src/test/kotlin/aoc2023/Day17Test.kt
@@ -14,16 +14,7 @@ class Day17Test {
 
     @Test
     fun `part1 example`() {
-        assertEquals(102, day17.part1(INPUT))
-    }
-
-    @Test
-    fun `part2 example`() {
-        assertEquals(0, day17.part2(INPUT))
-    }
-
-    private companion object {
-        val INPUT = """
+        val input = """
             2413432311323
             3215453535623
             3255245654254
@@ -38,5 +29,41 @@ class Day17Test {
             2546548887735
             4322674655533
         """.trimIndent()
+
+        assertEquals(102, day17.part1(input))
+    }
+
+    @Test
+    fun `part2 example - toy`() {
+        val input = """
+            111111111111
+            999999999991
+            999999999991
+            999999999991
+            999999999991
+        """.trimIndent()
+
+        assertEquals(71, day17.part2(input))
+    }
+
+    @Test
+    fun `part2 example - full`() {
+        val input = """
+            2413432311323
+            3215453535623
+            3255245654254
+            3446585845452
+            4546657867536
+            1438598798454
+            4457876987766
+            3637877979653
+            4654967986887
+            4564679986453
+            1224686865563
+            2546548887735
+            4322674655533
+        """.trimIndent()
+
+        assertEquals(94, day17.part2(input))
     }
 }
