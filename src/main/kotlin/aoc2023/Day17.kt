da5a5e415a9547af21ee399b178dbbdd291959e4
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2023-12-17 14:45:33 +0100

    refactor: Simplify part 1.

--- src/main/kotlin/aoc2023/Day17.kt
+++ src/main/kotlin/aoc2023/Day17.kt
@@ -1,7 +1,6 @@
 package aoc2023
 
 import java.util.PriorityQueue
-import kotlin.math.min
 
 class Day17 {
     fun part1(input: String): Int {
@@ -16,19 +15,19 @@ class Day17 {
     }
 
     private fun dijkstra(grid: Map<Point, Int>, source: Point, target: Point): Int {
-        val costs = mutableMapOf(StreakState(source, Point(0, 0), 0) to 0)
-        val path = mutableMapOf<Point, Point>()
+        val sourceState = StreakState(source, source, 0)
+        val costs = mutableMapOf(sourceState to 0)
         val pq = PriorityQueue<QueueItem>()
-        pq.add(QueueItem(source, 0, source, 0))
+        pq.add(QueueItem(sourceState, 0))
 
         while (pq.isNotEmpty()) {
             val item = pq.poll()
-            if (item.point == target) return costs[item.toStreakState()] ?: -1
+            if (item.state.point == target) return costs[item.state] ?: -1
             val nextItems = calculateNextItems(grid, item)
             for (nextItem in nextItems) {
-                val currCost = costs.getOrDefault(nextItem.toStreakState(), Int.MAX_VALUE)
+                val currCost = costs.getOrDefault(nextItem.state, Int.MAX_VALUE)
                 if (nextItem.cost < currCost) {
-                    costs[nextItem.toStreakState()] = nextItem.cost
+                    costs[nextItem.state] = nextItem.cost
                     pq.add(nextItem)
                 }
             }
@@ -38,15 +37,16 @@ class Day17 {
 
     private fun calculateNextItems(grid: Map<Point, Int>, item: QueueItem): List<QueueItem> {
         val nextItems = mutableListOf<QueueItem>()
-        val diff = item.point - item.prevPoint
+        val diff = item.state.point - item.state.prevPoint
         val neighbors = listOf(Pair(-1, 0), Pair(1, 0), Pair(0, -1), Pair(0, 1))
 
         for (neighbor in neighbors) {
-            if (neighbor == diff && item.streak == 3) continue
-            val newPoint = item.point + neighbor
-            if (newPoint == item.prevPoint || !grid.containsKey(newPoint)) continue
-            val streak = if (neighbor == diff) item.streak + 1 else 1
-            nextItems.add(QueueItem(newPoint, item.cost + grid[newPoint]!!, item.point, streak))
+            if (neighbor == diff && item.state.streak == 3) continue
+            val newPoint = item.state.point + neighbor
+            if (newPoint == item.state.prevPoint || !grid.containsKey(newPoint)) continue
+            val newStreak = if (neighbor == diff) item.state.streak + 1 else 1
+            val newState = StreakState(newPoint, item.state.point, newStreak)
+            nextItems.add(QueueItem(newState, item.cost + grid[newPoint]!!))
         }
         return nextItems
     }
@@ -62,13 +62,10 @@ class Day17 {
         return grid
     }
 
-    data class QueueItem(val point: Point, val cost: Int, val prevPoint: Point, val streak: Int) :
-        Comparable<QueueItem> {
+    data class QueueItem(val state: StreakState, val cost: Int) : Comparable<QueueItem> {
         override fun compareTo(other: QueueItem): Int {
             return cost.compareTo(other.cost)
         }
-
-        fun toStreakState() = StreakState(point, prevPoint, streak)
     }
 
     data class StreakState(val point: Point, val prevPoint: Point, val streak: Int)
