94945863f2de4a2c727711fc8f77e45bfd0f4907
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2023-12-17 15:20:17 +0100

    feat: Solution for Day 17, part 2.

--- src/main/kotlin/aoc2023/Day17.kt
+++ src/main/kotlin/aoc2023/Day17.kt
@@ -4,17 +4,16 @@ import java.util.PriorityQueue
 
 class Day17 {
     fun part1(input: String): Int {
-        val grid = parseInput(input)
-        val maxY = grid.keys.maxOf { it.y }
-        val maxX = grid.keys.maxOf { it.x }
-        return dijkstra(grid, Point(0, 0), Point(maxY, maxX))
+        val (grid, rows, cols) = parseInput(input)
+        return dijkstra(grid, Point(0, 0), Point(rows - 1, cols - 1), 1, 3)
     }
 
     fun part2(input: String): Int {
-        return 0
+        val (grid, rows, cols) = parseInput(input)
+        return dijkstra(grid, Point(0, 0), Point(rows - 1, cols - 1), 4, 10)
     }
 
-    private fun dijkstra(grid: Map<Point, Int>, source: Point, target: Point): Int {
+    private fun dijkstra(grid: Map<Point, Int>, source: Point, target: Point, minStreak: Int, maxStreak: Int): Int {
         val sourceState = StreakState(source, source, 0)
         val costs = mutableMapOf(sourceState to 0)
         val pq = PriorityQueue<QueueItem>()
@@ -23,7 +22,7 @@ class Day17 {
         while (pq.isNotEmpty()) {
             val item = pq.poll()
             if (item.state.point == target) return costs[item.state] ?: -1
-            val nextItems = calculateNextItems(grid, item)
+            val nextItems = calculateNextItems(grid, item, minStreak, maxStreak)
             for (nextItem in nextItems) {
                 val currCost = costs.getOrDefault(nextItem.state, Int.MAX_VALUE)
                 if (nextItem.cost < currCost) {
@@ -35,13 +34,26 @@ class Day17 {
         return -1
     }
 
-    private fun calculateNextItems(grid: Map<Point, Int>, item: QueueItem): List<QueueItem> {
+    private fun calculateNextItems(grid: Map<Point, Int>, item: QueueItem, minStreak: Int, maxStreak: Int): List<QueueItem> {
         val nextItems = mutableListOf<QueueItem>()
         val diff = item.state.point - item.state.prevPoint
+        if (item.state.streak in  1..<minStreak) {
+            val streakDiff = minStreak - item.state.streak
+            val nextPoint = item.state.point + diff.scale(streakDiff)
+            if (grid.containsKey(nextPoint)) {
+                val diffCost = IntRange(1, streakDiff).sumOf {
+                    val p = item.state.point + diff.scale(it)
+                    grid[p]!!
+                }
+                return listOf(QueueItem(StreakState(nextPoint, nextPoint - diff, minStreak), item.cost + diffCost))
+            }
+            return listOf()
+        }
+
         val neighbors = listOf(Pair(-1, 0), Pair(1, 0), Pair(0, -1), Pair(0, 1))
 
         for (neighbor in neighbors) {
-            if (neighbor == diff && item.state.streak == 3) continue
+            if (neighbor == diff && item.state.streak == maxStreak) continue
             val newPoint = item.state.point + neighbor
             if (newPoint == item.state.prevPoint || !grid.containsKey(newPoint)) continue
             val newStreak = if (neighbor == diff) item.state.streak + 1 else 1
@@ -51,7 +63,7 @@ class Day17 {
         return nextItems
     }
 
-    private fun parseInput(input: String): Map<Point, Int> {
+    private fun parseInput(input: String): Triple<Map<Point, Int>, Int, Int> {
         val grid = mutableMapOf<Point, Int>()
         val lines = input.split('\n').filter { it.isNotEmpty() }
         for (y in lines.indices) {
@@ -59,7 +71,7 @@ class Day17 {
                 grid[y to x] = lines[y][x].digitToInt()
             }
         }
-        return grid
+        return Triple(grid, lines.size, lines[0].length)
     }
 
     data class QueueItem(val state: StreakState, val cost: Int) : Comparable<QueueItem> {
--- src/main/kotlin/aoc2023/Utils.kt
+++ src/main/kotlin/aoc2023/Utils.kt
@@ -8,6 +8,7 @@ val Point.y get() = this.first
 val Point.x get() = this.second
 operator fun Point.plus(other: Point) = Point(y + other.y, x + other.x)
 operator fun Point.minus(other: Point) = Point(y - other.y, x - other.x)
+fun Point.scale(value: Int) = Point(y * value, x * value)
 
 fun readInputAsString(filename: String): String = getFile(filename).readText()
 
