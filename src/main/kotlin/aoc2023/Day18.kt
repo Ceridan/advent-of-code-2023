87ea5f899543f8c49e0b894d11d27cdfb80f2849
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2023-12-18 20:05:23 +0100

    feat: Solution for Day 18, part 2.

--- src/main/kotlin/aoc2023/Day18.kt
+++ src/main/kotlin/aoc2023/Day18.kt
@@ -3,60 +3,40 @@ package aoc2023
 class Day18 {
     fun part1(input: List<String>): Long {
         val instructions = parseInput(input).map { it.instruction }
-        var currentPoint = 0 to 0
-        val digSite = mutableListOf(currentPoint)
-        for (instruction in instructions) {
-            LongRange(1, instruction.distance).forEach { _ ->
-                currentPoint += instruction.directionAsPoint()
-                digSite.add(currentPoint)
-            }
-        }
-        return calculateInterior(digSite)
+        return calculateInterior(instructions)
     }
 
     fun part2(input: List<String>): Long {
         val instructions = parseInput(input).map { it.decodeColor() }
-        return 0
-    }
-
-    private fun calculateInterior(digSite: List<Point>): Long {
-        val area = calculateArea(digSite)
-        val border = digSite.size.toLong()
-        val inner = calculateInnerPoints(border, area)
-        return (border + inner).toLong()
+        return calculateInterior(instructions)
     }
 
     private fun calculateInterior(instructions: List<DigInstruction>): Long {
-        val area = calculateArea(instructions)
-        val border = instructions.sumOf { it.distance } + 1L
-        val inner = calculateInnerPoints(border, area)
-        return (border + inner).toLong()
+        val doubleArea = calculateDoubleArea(instructions)
+        val border = instructions.sumOf { it.distance }
+        val inner = calculateInnerPoints(border, doubleArea)
+        return border + inner
     }
+
+    // https://en.wikipedia.org/wiki/Pick%27s_theorem
+    private fun calculateInnerPoints(borderSize: Long, doubleArea: Long): Long = (doubleArea - borderSize + 2) / 2
 
     // https://en.wikipedia.org/wiki/Shoelace_formula
-    private fun calculateArea(digSite: List<Point>): Double {
-        val doubleArea = (digSite + listOf(digSite[0])).windowed(2).sumOf { (p1, p2) ->  1L * p1.x * p2.y - p1.y * p2.x }
-        return doubleArea / 2.0
-    }
-
-    private fun calculateArea(instructions: List<DigInstruction>): Double {
+    private fun calculateDoubleArea(instructions: List<DigInstruction>): Long {
         var detSum = 0L
         var point = Point(0, 0)
         for (instruction in instructions) {
             detSum += when (instruction.direction) {
-                'R' -> point.y * (1 - instruction.distance)
-                'D' -> point.x * (instruction.distance - 1)
-                'L' -> point.y * (instruction.distance - 1)
-                'U' -> point.x * (1 - instruction.distance)
+                'R' -> point.y * (-instruction.distance)
+                'D' -> point.x * instruction.distance
+                'L' -> point.y * instruction.distance
+                'U' -> point.x * (-instruction.distance)
                 else -> throw IllegalArgumentException("Unknown direction ${instruction.direction}")
             }
             point += instruction.directionAsPoint().scale(instruction.distance.toInt())
         }
-        return detSum / 2.0
+        return detSum
     }
-
-    // https://en.wikipedia.org/wiki/Pick%27s_theorem
-    private fun calculateInnerPoints(borderSize: Long, area: Double): Double = (area - borderSize / 2.0 + 1)
 
     private fun parseInput(input: List<String>): List<ColoredDigInstruction> {
         val instructions = mutableListOf<ColoredDigInstruction>()
@@ -69,7 +49,7 @@ class Day18 {
     }
 
     data class DigInstruction(val direction: Char, val distance: Long) {
-        fun directionAsPoint(): Point = when(direction) {
+        fun directionAsPoint(): Point = when (direction) {
             'R' -> 0 to 1
             'D' -> 1 to 0
             'L' -> 0 to -1
@@ -81,7 +61,7 @@ class Day18 {
     data class ColoredDigInstruction(val instruction: DigInstruction, val color: String) {
         fun decodeColor(): DigInstruction {
             val distance = color.drop(1).dropLast(1)
-            val direction = when(color.last()) {
+            val direction = when (color.last()) {
                 '0' -> 'R'
                 '1' -> 'D'
                 '2' -> 'L'
--- src/test/kotlin/aoc2023/Day18Test.kt
+++ src/test/kotlin/aoc2023/Day18Test.kt
@@ -13,17 +13,20 @@ class Day18Test {
     }
 
     @Test
-    fun `part1 example`() {
-        assertEquals(62L, day18.part1(INPUT))
+    fun `part1 example - toy`() {
+        val input = listOf(
+            "R 2 (#000020)",
+            "D 2 (#000021)",
+            "L 2 (#000022)",
+            "U 2 (#000023)",
+        )
+
+        assertEquals(9L, day18.part1(input))
     }
 
     @Test
-    fun `part2 example`() {
-        assertEquals(952408144115L, day18.part2(INPUT))
-    }
-
-    private companion object {
-        val INPUT = listOf(
+    fun `part1 example - full`() {
+        val input = listOf(
             "R 6 (#70c710)",
             "D 5 (#0dc571)",
             "L 2 (#5713f0)",
@@ -39,5 +42,41 @@ class Day18Test {
             "L 2 (#015232)",
             "U 2 (#7a21e3)",
         )
+
+        assertEquals(62L, day18.part1(input))
+    }
+
+    @Test
+    fun `part2 example - toy`() {
+        val input = listOf(
+            "R 2 (#000020)",
+            "D 2 (#000021)",
+            "L 2 (#000022)",
+            "U 2 (#000023)",
+        )
+
+        assertEquals(9L, day18.part2(input))
+    }
+
+    @Test
+    fun `part2 example - full`() {
+        val input = listOf(
+            "R 6 (#70c710)",
+            "D 5 (#0dc571)",
+            "L 2 (#5713f0)",
+            "D 2 (#d2c081)",
+            "R 2 (#59c680)",
+            "D 2 (#411b91)",
+            "L 5 (#8ceee2)",
+            "U 2 (#caa173)",
+            "L 1 (#1b58a2)",
+            "U 2 (#caa171)",
+            "R 2 (#7807d2)",
+            "U 3 (#a77fa3)",
+            "L 2 (#015232)",
+            "U 2 (#7a21e3)",
+        )
+
+        assertEquals(952408144115L, day18.part2(input))
     }
 }
