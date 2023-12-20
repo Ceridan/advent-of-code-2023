886dc06aa6e693d26aaf154dd783dba806845ba4
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2023-12-20 08:34:52 +0100

    chore: Generate day 20.

---
+++ src/main/kotlin/aoc2023/Day20.kt
@@ -0,0 +1,19 @@
+package aoc2023
+
+class Day20 {
+    fun part1(input: String): Long {
+        return 0
+    }
+
+    fun part2(input: String): Long {
+        return 0
+    }
+}
+
+fun main() {
+    val day20 = Day20()
+    val input = readInputAsString("day20.txt")
+
+    println("20, part 1: ${day20.part1(input)}")
+    println("20, part 2: ${day20.part2(input)}")
+}
---
+++ src/test/kotlin/aoc2023/Day20Test.kt
@@ -0,0 +1,45 @@
+package aoc2023
+
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day20Test {
+    private lateinit var day20: Day20
+
+    @BeforeEach
+    fun setUp() {
+        day20 = Day20()
+    }
+
+    @Test
+    fun `part1 example - 1 step cycle`() {
+        val input = """
+            broadcaster -> a, b, c
+            %a -> b
+            %b -> c
+            %c -> inv
+            &inv -> a
+        """.trimIndent()
+
+        assertEquals(32000000L, day20.part1(input))
+    }
+
+    @Test
+    fun `part1 example - 4 step cycle`() {
+        val input = """
+            broadcaster -> a
+            %a -> inv, con
+            &inv -> b
+            %b -> con
+            &con -> output
+        """.trimIndent()
+
+        assertEquals(11687500L, day20.part1(input))
+    }
+
+    @Test
+    fun `part2 example`() {
+        assertEquals(0, day20.part2(""))
+    }
+}
