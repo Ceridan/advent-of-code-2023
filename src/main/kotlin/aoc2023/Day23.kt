f0f1c88b8afd614bf9d66df66988f55aa6d7e35e
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2023-12-23 17:11:08 +0100

    refactor: Polished solution for day 23.

--- src/main/kotlin/aoc2023/Day23.kt
+++ src/main/kotlin/aoc2023/Day23.kt
@@ -5,63 +5,24 @@ import kotlin.math.max
 class Day23 {
     fun part1(input: String): Int {
         val (grid, start, end) = parseInput(input)
-        return dfs(grid, start, end) - 1
+        val graph = grid.convertToGraph().compactGraph()
+        return dfs(graph, start, end)
     }
 
     fun part2(input: String): Int {
         val (grid, start, end) = parseInput(input)
-        val graph = grid.convertToGraph()
-        val compactedGraph = graph.compactGraph()
-//        val gridWithoutSlopes = grid.mapValues { (_, ch) ->
-//            when (ch) {
-//                '^' -> '.'
-//                '>' -> '.'
-//                'v' -> '.'
-//                '<' -> '.'
-//                else -> ch
-//            }
-//        }
-//        return dfs(gridWithoutSlopes, start, end) - 1
-        return dfs2(compactedGraph, start, end, 0)
+        val graph = grid
+            .mapValues { (_, ch) -> if (ch in ">v<^") '.' else ch }
+            .convertToGraph()
+            .compactGraph()
+        return dfs(graph, start, end)
     }
 
     private fun dfs(
-        grid: Map<Point, Char>,
+        graph: Map<Point, List<Edge>>,
         current: Point,
         end: Point,
         steps: Int = 0,
-        visited: MutableSet<Point> = mutableSetOf(),
-    ): Int {
-        if (current == end) return steps + 1
-        if (current in visited) return 0
-
-        val ch = grid.getOrDefault(current, '#')
-        if (ch == '#') return 0
-
-        return when (ch) {
-            '^' ->  dfs(grid, current + (-1 to 0), end, steps + 1, visited)
-            '>' ->  dfs(grid, current + (0 to 1), end, steps + 1, visited)
-            'v' ->  dfs(grid, current + (1 to 0), end, steps + 1, visited)
-            '<' ->  dfs(grid, current + (0 to -1), end, steps + 1, visited)
-            else -> {
-                visited.add(current)
-                val longestPath = maxOf(
-                     dfs(grid, current + (-1 to 0), end, steps + 1, visited),
-                     dfs(grid, current + (0 to 1), end, steps + 1, visited),
-                     dfs(grid, current + (1 to 0), end, steps + 1, visited),
-                     dfs(grid, current + (0 to -1), end, steps + 1, visited)
-                )
-                visited.remove(current)
-                longestPath
-            }
-        }
-    }
-
-    private fun dfs2(
-        graph: Map<Point, Vertex>,
-        current: Point,
-        end: Point,
-        steps: Int,
         visited: MutableSet<Point> = mutableSetOf()
     ): Int {
         if (current == end) return steps
@@ -69,68 +30,52 @@ class Day23 {
 
         var longestPath = 0
         visited.add(current)
-        for (edge in graph[current]!!.edges) {
-            longestPath = max(longestPath, dfs2(graph, edge.v2, end, steps + edge.weight, visited))
+        for (edge in graph[current]!!) {
+            longestPath = max(longestPath, dfs(graph, edge.v2, end, steps + edge.weight, visited))
         }
         visited.remove(current)
         return longestPath
     }
 
-    private fun Map<Point, Char>.convertToGraph(): Map<Point, Vertex> {
-        val graph = mutableMapOf<Point, Vertex>()
+    private fun Map<Point, Char>.convertToGraph(): Map<Point, List<Edge>> {
+        val graph = mutableMapOf<Point, List<Edge>>()
         for ((point, ch) in this) {
             if (ch == '#') continue
 
             val edges = mutableListOf<Edge>()
-            for (direction in listOf(Point(-1, 0), Point(0, 1), Point(1, 0), Point(0, -1))) {
+            val directions = when (ch) {
+                '>' -> listOf(Point(0, 1))
+                'v' -> listOf(Point(1, 0))
+                '<' -> listOf(Point(0, -1))
+                '^' -> listOf(Point(-1, 0))
+                else -> listOf(Point(-1, 0), Point(0, 1), Point(1, 0), Point(0, -1))
+            }
+
+            for (direction in directions) {
                 val adj = point + direction
                 if (this.getOrDefault(adj, '#') == '#') continue
                 edges.add(Edge(point, adj))
             }
-            graph[point] = Vertex(point, edges)
+            graph[point] = edges
         }
         return graph
     }
 
-    private fun Map<Point, Vertex>.compactGraph(): Map<Point, Vertex> {
+    private fun Map<Point, List<Edge>>.compactGraph(): Map<Point, List<Edge>> {
         val newGraph = this.toMutableMap()
         for (point in this.keys) {
-            val vertex = newGraph[point]!!
-            if (vertex.edges.size != 2) continue
+            val edges = newGraph[point]!!
+            if (edges.size != 2) continue
 
-            val (_, p0, w0) = vertex.edges[0]
-            val (_, p1, w1) = vertex.edges[1]
-            val prevW0 = newGraph[p0]!!.edges.firstOrNull { it.v2 == p1 }?.weight ?: 0
-            val prevW1 = newGraph[p1]!!.edges.firstOrNull { it.v2 == p0 }?.weight ?: 0
+            val (_, p0, w0) = edges[0]
+            val (_, p1, w1) = edges[1]
+            val prevW0 = newGraph[p0]!!.firstOrNull { it.v2 == p1 }?.weight ?: 0
+            val prevW1 = newGraph[p1]!!.firstOrNull { it.v2 == p0 }?.weight ?: 0
             val newEdge0 = Edge(p0, p1, max(w0 + w1, prevW0))
             val newEdge1 = Edge(p1, p0, max(w0 + w1, prevW1))
-            newGraph[p0] = Vertex(p0, newGraph[p0]!!.edges.filter { it.v2 != point && it.v2 != p1 } + listOf(newEdge0))
-            newGraph[p1] = Vertex(p1, newGraph[p1]!!.edges.filter { it.v2 != point && it.v2 != p0 } + listOf(newEdge1))
+            newGraph[p0] = newGraph[p0]!!.filter { it.v2 != point && it.v2 != p1 } + listOf(newEdge0)
+            newGraph[p1] = newGraph[p1]!!.filter { it.v2 != point && it.v2 != p0 } + listOf(newEdge1)
             newGraph.remove(point)
-        }
-        return newGraph
-    }
-
-    private fun Map<Point, Vertex>.compactGraph2(): Map<Point, Vertex> {
-        val newGraph = this.toMutableMap()
-        val sortedPoints = newGraph.keys.sortedWith(compareBy({it.first}, {it.second}))
-        val queue = ArrayDeque(sortedPoints)
-        while (queue.isNotEmpty()) {
-            val point = queue.removeFirst()
-            if (point !in newGraph) continue
-            val vertex = newGraph[point]!!
-            if (vertex.edges.size != 2) continue
-
-            val (_, p0, w0) = vertex.edges[0]
-            val (_, p1, w1) = vertex.edges[1]
-            val prevW0 = newGraph[p0]!!.edges.firstOrNull { it.v2 == p1 }?.weight ?: 0
-            val prevW1 = newGraph[p1]!!.edges.firstOrNull { it.v2 == p0 }?.weight ?: 0
-            val newEdge0 = Edge(p0, p1, max(max(w0, w1) + 1, prevW0))
-            val newEdge1 = Edge(p1, p0, max(max(w0, w1) + 1, prevW1))
-            newGraph[p0] = Vertex(p0, newGraph[p0]!!.edges.filter { it.v2 != point && it.v2 != p1 } + listOf(newEdge0))
-            newGraph[p1] = Vertex(p1, newGraph[p1]!!.edges.filter { it.v2 != point && it.v2 != p0 } + listOf(newEdge1))
-            newGraph.remove(point)
-            queue.addFirst(p1)
         }
         return newGraph
     }
@@ -154,7 +99,6 @@ class Day23 {
         return Triple(grid, start, end)
     }
 
-    data class Vertex (val point: Point, val edges: List<Edge>)
     data class Edge(val v1: Point, val v2: Point, val weight: Int = 1)
 }
 
