4f0f6125271a14bb122773828c3ae6b4f53f9793
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2023-12-24 13:42:17 +0100

    refactor: Replace for-loop with foreach

--- src/main/kotlin/aoc2023/Day24.kt
+++ src/main/kotlin/aoc2023/Day24.kt
@@ -39,10 +39,7 @@ class Day24 {
         val sdz = ctx.mkIntConst("sdz")
 
         // It is enough to use only 3 hails to get a complete system of equations: 6 unknown variables (stone) + 3 unknown points in time (per hail).
-        val limit = 3
-        for (i in 0..<limit) {
-            val hail = hailstones[i]
-
+        hailstones.take(3).withIndex().forEach { (i, hail) ->
             val t = ctx.mkIntConst("t$i")
 
             // Stone
